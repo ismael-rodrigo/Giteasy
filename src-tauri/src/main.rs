00eed2bfa62b7e52b2d3ad955cd5d581076aca0c
ismael-rodrigo <ismaelbrasil02@gmail.com>
2024-01-21 23:27:16 -0300

    Add hotKey to open window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,9 +7,10 @@
 use std::process::Command;
 use std::env;
 use tauri::{SystemTray,  CustomMenuItem, SystemTrayMenu, SystemTrayMenuItem, SystemTrayEvent, Manager};
-use tauri_plugin_positioner:: { Position, WindowExt};
-use window_vibrancy::apply_acrylic ;
+use tauri_plugin_positioner:: {Position,WindowExt};
+use window_vibrancy::apply_acrylic;
 use window_shadows::set_shadow;
+use tauri::GlobalShortcutManager;
 
 #[tauri::command]
 fn get_current_branch(path: &str) -> Result<String, ()> {
@@ -19,6 +20,7 @@ fn get_current_branch(path: &str) -> Result<String, ()> {
 
     Ok(String::from_utf8(result.stdout).unwrap())
 }
+
 #[tauri::command]
 fn checkout_branch(path: &str, branch: &str, pull: bool, create_branch: bool ) -> Result<String, String> {
     let mut git = Command::new("git");
@@ -97,9 +99,10 @@ fn main() {
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
     let hide = CustomMenuItem::new("hide".to_string(), "Hide");
     let tray_menu = SystemTrayMenu::new()
-    .add_item(quit)
-    .add_native_item(SystemTrayMenuItem::Separator)
-    .add_item(hide);
+        .add_item(quit)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(hide);
+
     let tray = SystemTray::new().with_menu(tray_menu);
 
 
@@ -107,25 +110,27 @@ fn main() {
         .plugin(tauri_plugin_positioner::init())
         .on_system_tray_event( |app , event: SystemTrayEvent| {
             tauri_plugin_positioner::on_tray_event(app, &event);
+
+            let window = app.get_window("main").unwrap();
+            let _ = window.move_window(Position::TrayCenter).unwrap();
+            apply_acrylic(&window, Some((0, 0, 0, 0))).expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+            set_shadow(&window, true).expect("Unsupported platform! 'set_shadow' is only supported on Windows");
+
             match event {
-            SystemTrayEvent::MenuItemClick { id, .. } => {
-                match id.as_str() {
-                    "quit" => {
-                    std::process::exit(0);
+                SystemTrayEvent::MenuItemClick { id, .. } => {
+                    match id.as_str() {
+                        "quit" => {
+                        std::process::exit(0);
+                        }
+                        "hide" => {
+                        let window = app.get_window("main").unwrap();
+                        window.hide().unwrap();
+                        }
+                        _ => {}
                     }
-                    "hide" => {
-                    let window = app.get_window("main").unwrap();
-                    window.hide().unwrap();
-                    }
-                    _ => {}
-                }
                 }
             
                 SystemTrayEvent::LeftClick { position: _ , size: _ , ..} => {
-                    let window = app.get_window("main").unwrap();
-                    let _ = window.move_window(Position::TrayCenter).unwrap();
-                    apply_acrylic(&window, Some((0, 0, 0, 0))).expect("Unsupported platform! 'apply_blur' is only supported on Windows");
-                    set_shadow(&window, true).expect("Unsupported platform! 'set_shadow' is only supported on Windows");
                     if window.is_visible().unwrap() {
                         window.hide().unwrap();
                     } else {
@@ -136,8 +141,22 @@ fn main() {
                 _ => {}
             }
         })
+        .setup(| app| {
+            let window = app.get_window("main").unwrap();
+            tray.with_tooltip("Giteasy").build(app).unwrap();
+
+            app.global_shortcut_manager().register("Ctrl+Shift+z", move || {
+                if window.is_visible().unwrap() {
+                    window.hide().unwrap();
+                } else {
+                    window.show().unwrap();
+                    window.set_focus().unwrap();
+                }
+            }).unwrap();
+        
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet, get_current_branch, checkout_branch, open_folder_vs_code])
-        .system_tray(tray)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
